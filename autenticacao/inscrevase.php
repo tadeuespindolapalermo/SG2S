0328402cf3c3640eb10489a7c83c630462b849a7
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-03-13 21:34:54 -0300

    Implementei o bloqueio de inclusão de usuários já cadastrados com validações customizadas nos campos do formulário de cadastro de usuários.

--- autenticacao/inscrevase.php
+++ autenticacao/inscrevase.php
@@ -1,3 +1,10 @@
+<?php
+
+	$erro_usuario = isset($_GET['erro_usuario']) ? $_GET['erro_usuario'] : 0;
+	$erro_email = isset($_GET['erro_email']) ? $_GET['erro_email'] : 0;
+
+ ?>
+
 <!DOCTYPE HTML>
 <html lang="pt-br">
 	<head>
@@ -50,10 +57,20 @@
 				<form method="post" action="registra_usuario.php" id="formCadastrarse">
 					<div class="form-group">
 						<input type="text" class="form-control" id="usuario" name="usuario" placeholder="Usuário" required="requiored">
+						<?php
+							if($erro_usuario) {
+								echo '<font color="#FF0000">Usuário já existe!</font>';
+							}
+						 ?>
 					</div>
 
 					<div class="form-group">
 						<input type="email" class="form-control" id="email" name="email" placeholder="Email" required="requiored">
+						<?php
+							if($erro_email) {
+								echo '<font color="#FF0000">E-mail já existe!</font>';
+							}
+						 ?>
 					</div>
 
 					<div class="form-group">
--- autenticacao/registra_usuario.php
+++ autenticacao/registra_usuario.php
@@ -9,6 +9,49 @@
     $objConexao = new Conexao();
     $link = $objConexao->conectar();
 
+    $usuario_existe = false;
+    $email_existe = false;
+
+    // Verifica se o usuário já existe na base de dados
+    $sql = "SELECT * FROM usuarios WHERE usuario = '$usuario'";
+
+    if ($resultado_id = mysqli_query($link, $sql)) {
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+        if(isset($dados_usuario['usuario'])) {
+            $usuario_existe = true;
+        }
+    } else {
+        echo 'Erro ao tentar localizar o registro de usuário!';
+    }
+
+    // Verifica se o e-mail já existe na base de dados
+    $sql = "SELECT * FROM usuarios WHERE email = '$email'";
+
+    if ($resultado_id = mysqli_query($link, $sql)) {
+        $dados_usuario = mysqli_fetch_array($resultado_id);
+        if(isset($dados_usuario['email'])) {
+            $email_existe = true;
+        }
+    } else {
+        echo 'Erro ao tentar localizar o registro de e-mail!';
+    }
+
+    if ($usuario_existe || $email_existe) {
+
+        $retorno_get = '';
+
+        if ($usuario_existe) {
+            $retorno_get.= "erro_usuario=1&";
+        }
+
+        if ($email_existe) {
+            $retorno_get.= "erro_email=1&";
+        }
+
+        header('Location: inscrevase.php?' . $retorno_get);
+        die();
+    }
+
     // String SQL de inserção de usuário
     $sql = "INSERT INTO usuarios(usuario, email, senha) VALUES ('$usuario', '$email', '$senha')";
 
