76df0cc3195ee012e1f8eae7ddd521974e79bb36
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-03-16 05:39:16 -0300

    Adaptei o AngularJS apenas para a página de login, criei arquivo php de usuários, coloquei o conteúdo do template-model do bootstrap em arquivo separado e adaptei o projeto para utilizar recuperação de conteúdo de páginas com PHP via GET.

--- js/app/app.js
+++ js/app/app.js
@@ -6,13 +6,13 @@ angular.module("sg2s", ['ngRoute']).config(function($routeProvider) {
 		templateUrl:"index.php",
 		controller: "indexController"
 	})
-    .when("/inscrevase",{
+    /*.when("/inscrevase",{
 		templateUrl:"autenticacao/inscrevase.php",
 		controller: "inscricaoController"
 	})
 	.when("/home",{
 		templateUrl:"view/home.php",
 		controller:"homeController"
-	})
+	})*/
 	.otherwise({redirectTo:"/"});
 })
