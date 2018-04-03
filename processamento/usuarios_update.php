5adee6810718c8c81b1ce0e073a62d6cd8880c13
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-04-03 15:22:27 -0300

    Migrei para PDO o update de usuários pelo administrador. Este processamento não depende mais do mysqli e nem da conexão com banco de dados sem PDO.

--- processamento/usuarios_update.php
+++ processamento/usuarios_update.php
@@ -1,6 +1,5 @@
 <?php
-
-    session_start();    
+    session_start();
 
     if($_SESSION['perfil_idperfil'] == 2) {
         unset($_SESSION['usuario']);
@@ -11,65 +10,86 @@
 
     if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        require_once('../db/Conexao.class.php');
+        require('../db/Config.inc.php');
+
+        // CONEXÃO COM PDO
+        $PDO = new Conn;
+        $conn = $PDO->Conectar();
 
         $strPerfil = $_POST['perfil'];
-
         if ($strPerfil == 'Administrador') {
             $strPerfil = 1;
         } elseif ($strPerfil == 'Coordenador') {
             $strPerfil = 2;
         }
 
+        // Dados recebidos do formulário via POST
         $strNome = $_POST['nome'];
         $strTelefone = $_POST['telefone'];
         $strUsuario = $_POST['usuario'];
         $strEmail = $_POST['email'];
         $strSenhaNormal = $_POST['senha'];
         $strSenha = md5($_POST['senha']);
+        $strIdUsuario = $_GET['idUsuario'];
 
-        $objConexao = new Conexao();
-        $link1 = $objConexao->conectar();
-        $link2 = $objConexao->conectar();
+        try {
+            // UPDATE DO USUÁRIO
+            $strSqlUsuario = "
+            UPDATE usuarios set
+                nome = :nome,
+                fone = :telefone,
+                usuario = :usuario,
+                email = :email,
+                senha = :senha
+            WHERE
+                idusuarios = :idUsuario";
 
-        $strSqlUsuario = "
-        UPDATE usuarios set
-            nome = '".$strNome."',
-            fone = '".$strTelefone."',
-            usuario = '".$strUsuario."',
-            email = '".$strEmail."',
-            senha = '".$strSenha."'
-        WHERE
-            idusuarios = ".$_GET['idUsuario'];
+            $stmtUpdateUsuario = $conn->prepare($strSqlUsuario);
+            $stmtUpdateUsuario->bindParam(':nome', $strNome);
+            $stmtUpdateUsuario->bindParam(':telefone', $strTelefone);
+            $stmtUpdateUsuario->bindParam(':email', $strEmail);
+            $stmtUpdateUsuario->bindParam(':usuario', $strUsuario);
+            $stmtUpdateUsuario->bindParam(':senha', $strSenha);
+            $stmtUpdateUsuario->bindParam(':idUsuario', $strIdUsuario);
+            $updadeUsuario = $stmtUpdateUsuario->execute();
+            // -----------------------------------------------------------
 
-        $strSqlPerfilUsuario = "
-        UPDATE usuario_perfil set
-            perfil_idperfil = '".$strPerfil."'
-        WHERE
-            usuarios_idusuarios = ".$_GET['idUsuario'];
+            // UPDATE DO PERFIL DO USUÁRIO
+            $strSqlPerfilUsuario = "
+            UPDATE usuario_perfil set
+                perfil_idperfil = :idPerfil
+            WHERE
+                usuarios_idusuarios = :idUsuario";
 
-        $objConexao->atualizarUsuario($link1, $strSqlPerfilUsuario);
-        $objConexao->atualizarUsuario($link2, $strSqlUsuario);
+            $stmtUpdatePerfilUsuario = $conn->prepare($strSqlPerfilUsuario);
+            $stmtUpdatePerfilUsuario->bindParam(':idPerfil', $strPerfil);
+            $stmtUpdatePerfilUsuario->bindParam(':idUsuario', $strIdUsuario);
+            $updadePerfilUsuario = $stmtUpdatePerfilUsuario->execute();
+            // ------------------------------------------------------------
 
-        if ((mysqli_affected_rows($link1)) || (mysqli_affected_rows($link2)) > 0) {
+            // VALIDAÇÃO DO UPDATE
+            if ($updadeUsuario && $updadePerfilUsuario) {
 
-            $_SESSION['senha'] = $strSenhaNormal;
+                $_SESSION['senha'] = $strSenhaNormal;
 
-            echo "
-            <script type=\"text/javascript\">
-                alert(\"Usuário atualizado com sucesso!\");
-            </script>
-            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
-            http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
-            header('Location: ../view/admin.php?pagina=usuarios_listagem.php');
-        } else {
-            echo "
-            <script type=\"text/javascript\">
-                alert(\"Erro ao atualizar o usuário! Modifique algum dado no formulário!\");
-            </script>
-            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
-            http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
-            header('Location: ../view/admin.php?pagina=usuarios_listagem.php');
+                echo "
+                <script type=\"text/javascript\">
+                    alert(\"Usuário atualizado com sucesso!\");
+                </script>
+                <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
+                http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
+                //header('Location: ../view/admin.php?pagina=usuarios_listagem.php');
+            } else {
+                echo "
+                <script type=\"text/javascript\">
+                    alert(\"Erro ao atualizar o usuário!\");
+                </script>
+                <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
+                http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
+                //header('Location: ../view/admin.php?pagina=usuarios_listagem.php');
+            }
+        } catch (PDOException $e) {
+            PHPErro($e->getCode(), $e->getMessage(), $e->getFile(), $e->getFile());
         }
     }
 ?>
