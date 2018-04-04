9dd12b109210eb766ec44e6de8e26b2d3db72df1
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-04-04 20:28:08 -0300

    Adicionei bloco try catch no processamento usuario_remove com PDO.

--- processamento/usuario_remove.php
+++ processamento/usuario_remove.php
@@ -16,25 +16,29 @@
     $PDO = new Conn;
     $conn = $PDO->Conectar();
 
-    $sqlIdUsuario = "DELETE FROM usuarios WHERE idusuarios = :idUsuario";
-    $selectIdUsuario = $conn->prepare($sqlIdUsuario);
-    $selectIdUsuario->bindValue(':idUsuario', $idUsuario);
-    $selectIdUsuario->execute();
+    try {
+        $sqlIdUsuario = "DELETE FROM usuarios WHERE idusuarios = :idUsuario";
+        $selectIdUsuario = $conn->prepare($sqlIdUsuario);
+        $selectIdUsuario->bindValue(':idUsuario', $idUsuario);
+        $selectIdUsuario->execute();
 
-    $linhas = $selectIdUsuario->rowCount();
+        $linhas = $selectIdUsuario->rowCount();
 
-    if ($linhas != 0) {
-        echo "
-        <script type=\"text/javascript\">
-            alert(\"Usuário excluído com sucesso!\");
-        </script>
-        <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
-        http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
-    } else {
-        echo "
-        <script type=\"text/javascript\">
-            alert(\"Erro ao excluir usuário!\");
-        </script>
-        <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
-        http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
+        if ($linhas != 0) {
+            echo "
+            <script type=\"text/javascript\">
+                alert(\"Usuário excluído com sucesso!\");
+            </script>
+            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
+            http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
+        } else {
+            echo "
+            <script type=\"text/javascript\">
+                alert(\"Erro ao excluir usuário!\");
+            </script>
+            <META HTTP-EQUIV=REFRESH CONTENT = '0;URL=
+            http://localhost/SG2S/view/admin.php?pagina=usuarios_listagem.php'";
+        }
+    } catch (PDOException $e) {
+        PHPErro($e->getCode(), $e->getMessage(), $e->getFile(), $e->getFile());
     }
