baf52eeba485950399d415e16bb8ca7fae60e121
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-04-04 22:01:46 -0300

    Migrei para PDO a view view_usuario_logado_update que apresenta os dados do usuário da sessão atual do sistema num formulário de visualização com opção de edição dos dados. Esta view não depende mais do mysqli e nem da conexão com banco de dados sem PDO.

--- view/view_usuario_logado_update.php
+++ view/view_usuario_logado_update.php
@@ -1,16 +1,17 @@
 <?php
     session_start();
 
-    require_once('../db/Conexao.class.php');
+    $erro_update = isset($_GET['erro_update']) ? $_GET['erro_update'] : 0;
+
+    require('../db/Config.inc.php');
 
     $idUsuarioSessao = $_SESSION['idusuarios'];
 
-    $objConexao = new Conexao();
-    $link = $objConexao->conectar();
+    // CONEXÃO COM PDO
+    $PDO = new Conn;
+    $conn = $PDO->Conectar();
 
-    $erro_update = isset($_GET['erro_update']) ? $_GET['erro_update'] : 0;
-
-    $strSql= "
+    $strSqlUsuario= "
     SELECT
         nome,
         fone,
@@ -20,14 +21,25 @@
     FROM
         usuarios
     WHERE
-        idusuarios='{$idUsuarioSessao}'";
+        idusuarios = :idUsuario";
 
-    $rs = $objConexao->executarConsulta($link, $strSql);
+    $selectUsuario = $conn->prepare($strSqlUsuario);
+    $selectUsuario->bindValue(':idUsuario', $idUsuarioSessao);
+    $selectUsuario->execute();
 
-    $linha = mysqli_fetch_array($rs, MYSQLI_ASSOC);
+    $linhaUsuario = $selectUsuario->fetchAll(PDO::FETCH_ASSOC);
+
+    foreach ($linhaUsuario as $dados) {
+        $nome = $dados['nome'];
+        $usuario = $dados['usuario'];
+        $email = $dados['email'];
+        $fone = $dados['fone'];
+    }
+
     if($erro_update) {
         echo '<div style="margin-left: 18px;"><font color="#FF0000"><strong>Erro ao atualizar! Modifique algum dado no formulário!</strong></font></div>';
     }
+
     echo '
     <div class="container">
         <h3 class="text-muted">Atualizar Dados Cadastrais</h3>
@@ -35,16 +47,16 @@
             <div class="form-group">
                 <div class="col-lg-13">
                     <label class="col-lg-12 control-label label-usuario">Nome</label>
-                    <input type="text" style="width: 300px; margin-bottom: -5px;" id="nome" name="nome" class="form-control" value="'.$linha['nome'].'" required disabled><br/>
+                    <input type="text" style="width: 300px; margin-bottom: -5px;" id="nome" name="nome" class="form-control" value="'.$nome.'" required disabled><br/>
 
                     <label class="col-lg-2 control-label label-usuario" >Telefone</label>
-                    <input type="text" style="width: 300px; margin-bottom: -5px;" id="telefone" name="telefone" class="form-control" value="'.$linha['fone'].'" required disabled><br/>
+                    <input type="text" style="width: 300px; margin-bottom: -5px;" id="telefone" name="telefone" class="form-control" value="'.$fone.'" required disabled><br/>
 
                     <label class="col-lg-2 control-label label-usuario">Usuário</label>
-                    <input type="text" style="width: 300px; margin-bottom: -5px;" id="usuario" name="usuario" class="form-control" value="'.$linha['usuario'].'" required disabled><br/>
+                    <input type="text" style="width: 300px; margin-bottom: -5px;" id="usuario" name="usuario" class="form-control" value="'.$usuario.'" required disabled><br/>
 
                     <label class="col-lg-2 control-label label-usuario">Email</label>
-                    <input type="email" style="width: 300px; margin-bottom: -5px;" id="email" name="email" class="form-control" value="'.$linha['email'].'" required disabled><br/>
+                    <input type="email" style="width: 300px; margin-bottom: -5px;" id="email" name="email" class="form-control" value="'.$email.'" required disabled><br/>
 
                     <label class="col-lg-2 control-label label-usuario">Senha</label>
                     <input type="password" style="width: 300px; margin-bottom: -5px;" id="senha" name="senha" class="form-control" value="'.$_SESSION['senha'].'" required disabled><br/>
