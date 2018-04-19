2dbdfa1136df1c1ffb16fa0b3fc3eef2e1f5d024
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-04-18 22:46:37 -0300

    Atualização do layout inicial da view de grade pós gerada, implementada manualmente para teste.

--- view/view_form_grade_gerar.php
+++ view/view_form_grade_gerar.php
@@ -28,18 +28,95 @@
 	<br /><br />
 
     <div class="col-md-4"></div>
-	<div class="col-md-4">
+	<div class="col-md-12">
 		<br />
 		<h3><strong><div style="margin-top: -50px;">Gerar Grade Semestral</div></strong></h3>
 		<br />
-		<form method="post" action="../controller/controller_form_grade_gerar.php" id="formGradeGerar">
-			<div class="form-group">
-				<small><strong>*Campos Obrigatórios</strong></small>
 
-
-
-			    <button type="submit" class="btn btn-primary form-control">Cadastrar</button>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-striped" id="listaUsuarios" border="1">
+                                <thead>
+                                    <tr>
+                                        <th>SEMESTRE<br>TURMA</th>
+                                        <th>Nª DE ALUNOS</th>
+                                        <th>SEGUNDA</th>
+                                        <th>TERÇA</th>
+                                        <th>QUARTA</th>
+                                        <th>QUINTA</th>
+                                        <th>SEXTA</th>
+                                        <th>SÁBADO</th>
+                                        <th>EAD</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <div style="text-align: center;"><p>FACULDADE JK</p></div>
+                                        <div style="text-align: center;"><p>GRADE HORÁRIA: TECNÓLOGO EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS - 1º SEMESTRE DE 2018</p></div>
+                                        <div style="text-align: center;"><p>Período: 19:10 às 20:00 / 20:00 às 20:50 / 21:10 às 22:00</p></div>
+                                        <td>TADS 1A</td>										
+                                        <td>Disciplinas</td>
+                                        <td>Lógica de Programação e Algoritmos</td>
+                                        <td>Geometria Anallítica e Agebra Linear</td>
+                                        <td>Organização e Arquitatura de Computadores</td>
+                                        <td>Metodologia Científica I</td>
+                                        <td>Língua Portuguesa - Gramática Básica, Leitura e Interpretação de Textos</td>
+                                        <td></td>
+                                        <td>Inglês Instrumental(EAD)</td>
+                                    </tr>
+									<tr>
+										<td>TADS 1A</td>
+                                        <td>Disciplinas</td>
+                                        <td>Lógica de<br> Programação e<br/> Algoritmos</td>
+                                        <td>Geometria Anallítica e Agebra Linear</td>
+                                        <td>Organização e Arquitatura de Computadores</td>
+                                        <td>Metodologia Científica I</td>
+                                        <td>Língua Portuguesa - Gramática Básica, Leitura e Interpretação de Textos</td>
+                                        <td></td>
+                                        <td>Inglês Instrumental(EAD)</td>
+									</tr>
+									<tr>
+										<td>TADS 1A</td>
+                                        <td>Disciplinas</td>
+                                        <td>Lógica de<br> Programação e<br/> Algoritmos</td>
+                                        <td>Geometria Anallítica e Agebra Linear</td>
+                                        <td>Organização e Arquitatura de Computadores</td>
+                                        <td>Metodologia Científica I</td>
+                                        <td>Língua Portuguesa - Gramática Básica, Leitura e Interpretação de Textos</td>
+                                        <td></td>
+                                        <td>Inglês Instrumental(EAD)</td>
+									</tr>
+									<tr>
+										<td>TADS 1A</td>
+                                        <td>Disciplinas</td>
+                                        <td>Lógica de<br> Programação e<br/> Algoritmos</td>
+                                        <td>Geometria Anallítica e Agebra Linear</td>
+                                        <td>Organização e Arquitatura de Computadores</td>
+                                        <td>Metodologia Científica I</td>
+                                        <td>Língua Portuguesa - Gramática Básica, Leitura e Interpretação de Textos</td>
+                                        <td></td>
+                                        <td>Inglês Instrumental(EAD)</td>
+									</tr>
+									<tr>
+										<td>TADS 1A</td>
+                                        <td>Disciplinas</td>
+                                        <td>Lógica de<br> Programação e<br/> Algoritmos</td>
+                                        <td>Geometria Anallítica e Agebra Linear</td>
+                                        <td>Organização e Arquitatura de Computadores</td>
+                                        <td>Metodologia Científica I</td>
+                                        <td>Língua Portuguesa - Gramática Básica, Leitura e Interpretação de Textos</td>
+                                        <td></td>
+                                        <td>Inglês Instrumental(EAD)</td>
+									</tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
-		</form>
+        </div>
     </div>
 </div>
