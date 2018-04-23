9cb8d967249d2815967b12941673e5c70590be93
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-04-22 21:38:27 -0300

    Migrei jQuery 1.9.1 de CDN para arquivo de biblioteca fixo no projeto.

--- view/view_admin.php
+++ view/view_admin.php
@@ -249,7 +249,11 @@
         <!-- jquery 3.2.1 sem cdn -->
         <script src="../lib/jquery/jquery-3_2_1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
-        <script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+        <!-- jquery 1.9.1 com cdn -->
+        <!--<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>-->
+
+        <!-- jquery 1.9.1 sem cdn -->
+        <script src="../lib/jquery/jquery-1_9_1.min.js"></script>-->
 
         <script>window.jQuery || document.write('<script src="../lib/jquery/jquery-slim.min.js"><\/script>')</script>
         <script src="../js/popper.min.js"></script>
