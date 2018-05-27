5a0a7e4a5b04ab360d5315f3011b570888d7e279
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2018-05-27 05:20:32 -0300

    Alterei a visualização da matriz de cursos, substitui tabela por janela modal bootstrap.

--- view/view_matriz_listagem.php
+++ view/view_matriz_listagem.php
@@ -42,33 +42,46 @@
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="table-responsive">
-                        <h3 class="text-muted">Matriz do Curso de <?php echo $curso->getNome(); ?></h3><hr />
-                        <table style="text-align: center; width: 400px; margin-left: 320px;" class="table table-hover table-striped listaSearch" id="listaMatriz">
-                            <thead>
-                                <tr>
-                                    <th>Disciplinas</th>
-                                </tr>
-                            </thead>
-                            <?php
-                                while ($linhaMatriz = $selectMatriz->fetchAll(PDO::FETCH_ASSOC)) {
-                                    foreach ($linhaMatriz as $dados) {
-                                        $disciplina->setNomeDisciplina($dados['nome_disciplina']);
-                                        echo '
-                                        <tbody>
-                                            <tr>
-                                                <td>'.$disciplina->getNomeDisciplina().'</td>
-                                            </tr>
-                                        </tbody>';
-                                    }
-                                }
-                            ?>
-                        </table>
+                        <h3 class="text-muted" style="text-align: center;">Matriz do Curso de <?php echo $curso->getNome(); ?></h3><hr />
+
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Matriz do Curso de <?php echo $curso->getNome(); ?> </h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <?php
+                                        while ($linhaMatriz = $selectMatriz->fetchAll(PDO::FETCH_ASSOC)) {
+                                            foreach ($linhaMatriz as $dados) {
+                                                $disciplina->setNomeDisciplina($dados['nome_disciplina']);
+                                                echo '<div style="font-size: 18px;">'.$disciplina->getNomeDisciplina().'</div>';
+                                            }
+                                        }
+                                        ?>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <br/>
                         <!--<button export-to-excel="listaMatriz" class="btn btn-success">
                             <span data-feather="download"></span>&nbsp;Excel
                         </button>-->
-                        <button id="btnSearch" onclick="alterarDisabledSearch()" class="btn btn-outline-dark"><span data-feather="search"></span>&nbsp;Busca</button>
-                        <a href="<?php echo $url;?>?pagina=view_cursos_listagem.php"><button type="button" class="btn btn-info"><span data-feather="arrow-left"></span>&nbsp;Voltar</button></a>
+
+                        <!-- Button trigger modal -->
+                        <button style="width: 200px; margin-left: 400px;" type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            Visualizar
+                        </button><br/><br/>
+
+                        <!--<button id="btnSearch" onclick="alterarDisabledSearch()" class="btn btn-outline-dark"><span data-feather="search"></span>&nbsp;Busca</button>-->
+                        <a href="<?php echo $url;?>?pagina=view_cursos_listagem.php"><button style="width: 200px; margin-left: 400px;" type="button" class="btn btn-info"><span data-feather="arrow-left"></span>&nbsp;Voltar</button></a>
                     </div><br /><hr>
                     <div id="conteudo"></div>
                 </div>
